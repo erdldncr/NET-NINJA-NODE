93ed56859e23cd40312a43777cd92ebd8f0c396e
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-05-06 00:48:35 +0100

    basic routing

--- server.js
+++ server.js
@@ -4,9 +4,26 @@ const server=http.createServer((req,res)=>{
     console.log(req.url,req.method);
     ////set header content type
     res.setHeader('Content-Type','html');///res type
+    
+    ///get path from browser
+    let path='./views';
+    switch(req.url){
+        case '/':{
+            path+='/index.html'
+            break;
 
+        }
+        case '/about':{
+            path+='/about.html'
+            break;
+        }
+        default :{
+            path+='/404.html'
+            break;
+        }
+    }
     //fs ile dosyayi okuyorum
-    fs.readFile('./views/index.html',(err,data)=>{
+    fs.readFile(path,(err,data)=>{
         ///hata varsa log yap
         if(err){
             console.log(err)
