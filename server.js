32ae2023051c1976afe0622b49fb8e572dbe1ec2
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-05-10 04:15:13 +0100

    basic routing without express

--- server.js
+++ server.js
@@ -10,15 +10,25 @@ const server=http.createServer((req,res)=>{
     switch(req.url){
         case '/':{
             path+='/index.html'
+            res.statusCode=200;
             break;
 
         }
         case '/about':{
             path+='/about.html'
+            res.statusCode=200;
+            break;
+        }
+        case '/about-me':{
+         ///this is to redirect to page
+            res.statusCode=301;
+            res.setHeader('Location','/about');
+            res.end()
             break;
         }
         default :{
             path+='/404.html'
+            res.statusCode=404;
             break;
         }
     }
@@ -33,6 +43,7 @@ const server=http.createServer((req,res)=>{
         //    res.write(data);
            ///respondu sonlandir
            res.end(data)
+           
         }
     })
 
